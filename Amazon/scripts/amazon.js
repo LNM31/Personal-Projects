854aedb47355763a0c7b439d9f6ac367a1fa3715
LNM31 <licanemes31@gmail.com>
2025-08-09 22:09:37 +0300

    cart button interactive

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -45,7 +45,8 @@ products.forEach((product) => {
                 Added
               </div>
 
-              <button class="add-to-cart-button button-primary">
+              <button class="add-to-cart-button button-primary js-add-to-cart"
+              data-product-id="${product.id}">
                 Add to Cart
               </button>
             </div>
@@ -53,3 +54,26 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click',() => {
+    const productId = button.dataset.productId; //data-product-id => converted to ProductId
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem) {
+      matchingItem.quantity++;
+    }else{
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }    
+    console.log(cart);
+  })
+});
