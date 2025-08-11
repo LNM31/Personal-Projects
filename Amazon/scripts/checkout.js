5803bf3985586554772200df9e759340f9351a75
LNM31 <licanemes31@gmail.com>
2025-08-11 17:58:15 +0300

    delete products from html

--- Amazon/scripts/checkout.js
+++ Amazon/scripts/checkout.js
@@ -19,7 +19,7 @@ cart.forEach((cartItem) => {
   console.log(matchingProduct);
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -104,5 +104,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const productId = link.dataset.productId;
     console.log(productId);
     removeFromCart(productId);
-  })
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
+  });
 });
