2695d5b0cf74abe327b220745aae0465c1953f8c
LNM31 <licanemes31@gmail.com>
2025-08-21 00:06:24 +0300

    working at orders page

--- Amazon/scripts/orders.js
+++ Amazon/scripts/orders.js
@@ -1,9 +1,108 @@
+import { getProduct,loadProductsFetch } from "../data/products.js"
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import formatCurrency from "./utils/money.js";
 import { orders } from "../data/orders.js";
+import { addToCart } from "../data/cart.js";
 
-function renderOrders()
+
+async function loadPage()
 {
-  const html = /*html*/ 
-  `
-    
-  `
+  await loadProductsFetch();
+
+  let html = '';
+
+  orders.forEach((order) => 
+  {
+    const orderTimingString = dayjs(order.orderTime).format('MMMM D');
+
+    html += /*html*/
+    `
+      <div class="order-header">
+        <div class="order-header-left-section">
+          <div class="order-date">
+            <div class="order-header-label">Order Placed:</div>
+            <div>${orderTimingString}</div>
+          </div>
+          <div class="order-total">
+            <div class="order-header-label">Total:</div>
+            <div>${formatCurrency(order.totalCostCents)}</div>
+          </div>
+        </div>
+
+        <div class="order-header-right-section">
+          <div class="order-header-label">Order ID:</div>
+          <div>${order.id}</div>
+        </div>
+      </div>
+
+      <div class="order-details-grid">
+        ${productsListHTML(order)}
+      </div>
+    `
+  });
+
+  document.querySelector('.js-orders-grid').innerHTML = html;
+
+  document.querySelectorAll('.js-buy-again').forEach((button) => 
+  {
+    button.addEventListener('click', () => 
+    {
+      addToCart(button.dataset.productId,1);
+
+      button.innerHTML = 'Added';
+      setTimeout(() => 
+      {
+        button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+      }, 1000);
+    })
+  });
 }
+
+function productsListHTML(order)
+{
+  let productsList = '';
+
+  order.products.forEach((productDetails) => 
+  {
+    const product = getProduct(productDetails.productId);
+
+    productsList += /*html*/
+    `
+      <div class="product-image-container">
+        <img src="${product.image}">
+      </div>
+
+      <div class="product-details">
+        <div class="product-name">
+          ${product.name}
+        </div>
+        <div class="product-delivery-date">
+          Arriving on: ${dayjs(productDetails.estimatedDeliveryTime).format('MMMM D')} 
+        </div>
+        <div class="product-quantity">
+          Quantity: ${productDetails.quantity}
+        </div>
+        <button class="buy-again-button button-primary js-buy-again" data-product-id="${product.id}">
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        </button>
+      </div>
+
+      <div class="product-actions">
+        <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
+          <button class="track-package-button button-secondary">
+            Track package
+          </button>
+        </a>
+      </div>
+    `;
+
+    
+  });
+  return productsList;
+}
+
+loadPage();
