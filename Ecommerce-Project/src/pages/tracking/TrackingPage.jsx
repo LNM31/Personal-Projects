036861cea4ee3f3277a5f27f4b5c9ecda16e4bb9
LNM31 <licanemes31@gmail.com>
2025-08-31 17:03:46 +0300

    replaced <a></a> with Link components

--- Ecommerce-Project/src/pages/tracking/TrackingPage.jsx
+++ Ecommerce-Project/src/pages/tracking/TrackingPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { Header } from "../../components/Header.jsx";
 import './TrackingPage.css'
 
@@ -12,9 +13,9 @@ export function TrackingPage()
 
       <div class="tracking-page">
         <div class="order-tracking">
-          <a class="back-to-orders-link link-primary" href="/orders">
+          <Link class="back-to-orders-link link-primary" to="/orders">
             View all orders
-          </a>
+          </Link>
 
           <div class="delivery-date">
             Arriving on Monday, June 13
