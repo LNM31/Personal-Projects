016435637ebdba69b64b04a744d5169f67000f24
LNM31 <licanemes31@gmail.com>
2025-09-02 23:39:08 +0300

    generate the html for the orders page

--- Ecommerce-Project/frontend/src/App.jsx
+++ Ecommerce-Project/frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage cart={cart} />} /> {/* index <=> path="/" */}
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-        <Route path="orders" element={<OrdersPage />} />
+        <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route path="tracking" element={<TrackingPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
