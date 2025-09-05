c40f47a386c0d05a1c1fb6f001dd30a0269ba475
LNM31 <licanemes31@gmail.com>
2025-09-05 11:42:06 +0300

    added cart to tracking and notFound page

--- Ecommerce-Project/frontend/src/App.jsx
+++ Ecommerce-Project/frontend/src/App.jsx
@@ -26,8 +26,8 @@ function App() {
         <Route index element={<HomePage cart={cart} />} /> {/* index <=> path="/" */}
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="orders" element={<OrdersPage cart={cart} />} />
-        <Route path="tracking" element={<TrackingPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="tracking" element={<TrackingPage cart={cart} />} />
+        <Route path="*" element={<NotFoundPage cart={cart} />} />
       </Routes>
     </>
   )
